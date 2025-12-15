570e619a72907ce9b8f90ff0c173491c8d98c6e4
anrrt <felix@DESKTOP-AMTQRQL.localdomain>
2025-12-15 13:32:49 +0100

    Testscripts deleted

--- scripts/TestFieStorage.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("videos.json", "r", encoding="utf-8") as file:
-    videoInformation = json.load(file)
-
-print(f"{len(videoInformation)} Videos geladen.")
-print(videoInformation[0]["title"])
